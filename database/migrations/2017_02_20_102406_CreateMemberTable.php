ef242196f81fb1265dfd6748bde511219f4395a8
Mehmet Uygun <mehmet.uygun@outlook.com>
2017-02-20 10:47:13 +0000

    Create member database migration

---
+++ database/migrations/2017_02_20_102406_CreateMemberTable.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+            $table->increments('id');
+            $table->first_name('first_name', 45);
+            $table->last_name('last_name', 45);
+            $table->email('email', 65);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member');
+    }
+}
